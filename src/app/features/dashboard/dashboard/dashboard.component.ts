6ec3e4c458a066512ad0f2056c923145da77c332
omarsalem7 <omarsalem0721@gmail.com>
2025-01-06 16:02:16 +0300

    Add TasksClassification component to dashboard; implement chart display and update layout

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { TasksClassificationComponent } from '../components/tasks-classification/tasks-classification.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TasksClassificationComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
