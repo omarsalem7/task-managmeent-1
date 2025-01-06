bab628e49dd7ece937e0b52d34b5f0c971b562fe
omarsalem7 <omarsalem0721@gmail.com>
2025-01-06 16:57:43 +0300

    Add AllTasks component to dashboard; implement doughnut chart for task overview

--- src/app/features/dashboard/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TasksClassificationComponent } from '../components/tasks-classification/tasks-classification.component';
+import { AllTasksComponent } from '../components/all-tasks/all-tasks.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TasksClassificationComponent],
+  imports: [TasksClassificationComponent, AllTasksComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
