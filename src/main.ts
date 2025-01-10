ec3dfc163c520a1015e4eb5f6c00656e40a158b6
omarsalem7 <omarsalem0721@gmail.com>
2025-01-10 14:24:26 +0300

    Enable production mode and suppress console output in production environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+  console.warn(`🚨 Console output is disabled on production!`);
+  console.log = function (): void {};
+  console.debug = function (): void {};
+  console.warn = function (): void {};
+  console.info = function (): void {};
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
