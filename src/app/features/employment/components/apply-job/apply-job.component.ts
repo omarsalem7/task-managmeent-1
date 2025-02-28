a8b4d4aebc18cdbfddd74f0b44aeabe66b78b927
omarsalem7 <omarsalem0721@gmail.com>
2025-03-01 00:50:23 +0300

    Add resume input field to ApplyJob component; integrate file upload handler and update validation

--- src/app/features/employment/components/apply-job/apply-job.component.ts
+++ src/app/features/employment/components/apply-job/apply-job.component.ts
@@ -6,11 +6,12 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { FileUploadHandlerComponent } from '../../../../shared/ui/file-upload-handler/file-upload-handler.component';
 
 @Component({
   selector: 'app-apply-job',
   standalone: true,
-  imports: [TranslateModule, ReactiveFormsModule],
+  imports: [TranslateModule, ReactiveFormsModule, FileUploadHandlerComponent],
   templateUrl: './apply-job.component.html',
   styleUrl: './apply-job.component.scss',
 })
@@ -22,6 +23,7 @@ export class ApplyJobComponent {
       fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern('^5[0-9]{8}$')]],
+      resume: ['', [Validators.required]],
     });
   }
 
