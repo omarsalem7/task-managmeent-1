1e910888e08b68b1bd44f59676e7bef3b7546757
omarsalem7 <omarsalem0721@gmail.com>
2025-03-02 11:52:24 +0300

    Remove unused token handling methods and add JobService for future job-related features

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -1,30 +1,27 @@
 // token.service.ts
 import { Injectable } from '@angular/core';
-import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
-  decodeToken(token: string): any {
-    try {
-      return jwtDecode(token);
-    } catch (error) {
-      console.error('Error decoding token:', error);
-      return null;
-    }
-  }
-
-  getUserInfo(token: string): any {
-    const decoded = this.decodeToken(token);
-    console.log(decoded);
-    return decoded ? decoded : null;
-  }
-
-  setRole() {
-    const currentUser = localStorage.getItem('currentUser');
-    const token = currentUser ? JSON.parse(currentUser).token : ''; // Replace with your storage method
-    const roleInfo = this.getUserInfo(token).role;
-    localStorage.setItem('role', roleInfo);
-  }
+  // decodeToken(token: string): any {
+  //   try {
+  //     return jwtDecode(token);
+  //   } catch (error) {
+  //     console.error('Error decoding token:', error);
+  //     return null;
+  //   }
+  // }
+  // getUserInfo(token: string): any {
+  //   const decoded = this.decodeToken(token);
+  //   console.log(decoded);
+  //   return decoded ? decoded : null;
+  // }
+  // setRole() {
+  //   const currentUser = localStorage.getItem('currentUser');
+  //   const token = currentUser ? JSON.parse(currentUser).token : ''; // Replace with your storage method
+  //   const roleInfo = this.getUserInfo(token).role;
+  //   localStorage.setItem('role', roleInfo);
+  // }
 }
